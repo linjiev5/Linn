dd5add9cc4903444752dc3ef8ac68dcc016e48ef
linjiev5 <70872452+linjiev5@users.noreply.github.com>
2020-10-11 00:43:05 +0900

    a
    aaa

---
+++ src/ｃ007/Pet.java
@@ -0,0 +1,13 @@
+package ｃ007;
+
+/**
+ * ペットクラスを作成する
+ *
+ * @author jie
+ *
+ */
+public class Pet {
+	String name;
+	int age;
+
+}
---
+++ src/ｃ007/Pet1.java
@@ -0,0 +1,10 @@
+package ｃ007;
+
+public class Pet1 {
+
+	public static void main(String[] args) {
+		int x = 110;
+		System.out.println(Integer.toHexString(x));
+	}
+
+}
